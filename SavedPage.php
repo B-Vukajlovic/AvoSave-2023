3f48c63681834fa8a3a9f4b7f3eae776c21ca3a3
Diederick de Gier <diederick.de.gier@student.uva.nl>
2024-01-24 13:58:55 +0100

    git no pulicly accessible + changes for email not changeable

--- SavedPage.php
+++ SavedPage.php
@@ -34,10 +34,11 @@
         <div class="mainpage">
             <h1 class = "title-page">My Saved Recipes</h1>
             <?php
+            //TODO: Cookie with UserID Needed in query.
                 $query = "SELECT R.Title AS RecipeTitle, GROUP_CONCAT(RI.IngredientName) AS Ingredients
                     FROM Recipe R, UserRecipe UR
                     JOIN RecipeIngredient AS RI ON R.RecipeID = RI.RecipeID
-                    WHERE R.RecipeID = $id AND UR.RecipeID AND UR.SavedStatus = 1
+                    WHERE R.RecipeID = UR.RecipeID AND UR.SavedStatus = 1
                     GROUP BY R.RecipeID";
             $result = pdo->query($query);
             while ($row = mysqli_fetch_assoc($result)) {
