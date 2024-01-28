55433f245c901818eb58fd4f037d9a7e3df18b3e
Diederick de Gier <diederick.de.gier@student.uva.nl>
2024-01-28 20:51:32 +0100

    Fixed issue recipes not displaying at start

--- filter_handeling.js
+++ filter_handeling.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
+  sendFilterRequest();
+
   $('input[type="checkbox"]').change(function() {
     sendFilterRequest();
   });
 });
+
+// Sends a request to apply the new filter
+function sendFilterRequest() {
+  // Gets all checked filters
+  var checkedCheckboxes = $('input[type="checkbox"]:checked');
+  var checkedValues = checkedCheckboxes.map(function() {
+    return this.value;
+  }).get();
+
+  // Sends AJAX request to update recipes based on changed filters
+  $.ajax({
+    url: 'process-filter-request.php',
+    type: 'POST',
+    data: { filtersApplied: checkedValues },
+    success: function(response) {
+      $('.recipes').html(response);
+    },
+    error: function(error) {
+      console.error('Error:', error);
+    }
+  });
+}
+
 
 // Sends a request to apply the new filter
 function sendFilterRequest() {
