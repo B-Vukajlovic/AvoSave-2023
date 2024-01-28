55baf4423d5bf200a8ea35a0383fa27b85a86704
Diederick de Gier <diederick.de.gier@student.uva.nl>
2024-01-28 20:45:50 +0100

    Implementation filters

--- filter_handeling.js
+++ filter_handeling.js
@@ -1,1 +1,28 @@
-let checkbox = document.getElementsByName("checkbox");
+$(document).ready(function() {
+  $('input[type="checkbox"]').change(function() {
+    sendFilterRequest();
+  });
+});
+
+// Sends a request to apply the new filter
+function sendFilterRequest() {
+  // Gets all checked filters
+  var checkedCheckboxes = $('input[type="checkbox"]:checked');
+  var checkedValues = checkedCheckboxes.map(function() {
+    return this.value;
+  }).get();
+
+    // Sends AJAX request to update recipes based on changed filters
+  $.ajax({
+    url: 'process-filter-request.php',
+    type: 'POST',
+    data: { filtersApplied: checkedValues },
+    success: function(response) {
+      $('.recipes').html(response);
+    },
+    error: function(error) {
+      console.error('Error:', error);
+    }
+  });
+
+}
