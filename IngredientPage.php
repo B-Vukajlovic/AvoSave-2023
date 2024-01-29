072d40b68844ea3dbc1de724590b1f9026c4eaff
Ismail Ouhamcha <ismail.ouhamcha@student.uva.nl>
2024-01-29 17:14:13 +0100

    small style change

--- IngredientPage.php
+++ IngredientPage.php
@@ -1,18 +1,6 @@
 <?php
-$dbname = "AvoSave";
-$dbuser = "ismailo";
-$dbpass = "ytrewq";
-$dbhost = "localhost";
-
-try{
-    $pdo = new PDO("mysql:host =" . $dbhost . ";dbname=" . $dbname, $dbuser, $dbpass);
-} catch (PDOException $e){
-    echo "Database error occurred: " . $e->getMessage();
-    exit();
-}
-
+require_once('pdo-connect.php');
 $ingredients = $pdo->query("SELECT Name, Type FROM Ingredient");
-
 ?>
 
 <!DOCTYPE html>
