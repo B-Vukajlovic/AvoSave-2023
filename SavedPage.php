80e96905b1ed35e77226fd8deb48dc322c252500
Diederick de Gier <diederick.de.gier@student.uva.nl>
2024-02-01 02:05:23 +0100

    Image changes

--- SavedPage.php
+++ SavedPage.php
@@ -32,17 +32,16 @@ if ($_SESSION['userid'] == null) {
             <h1 class = "title-page">My Saved Recipes</h1>
             <?php
             $UserID = $_SESSION["userid"];
-             //TO DO: Image fix needed
-             $query = "SELECT R.Title AS RecipeTitle, GROUP_CONCAT(RI.IngredientName) AS Ingredients
-                     FROM Recipe R, UserRecipe UR
+            $query = "SELECT R.Title AS RecipeTitle, GROUP_CONCAT(RI.IngredientName) AS Ingredients, ImageURL
+                     FROM Recipe R, UserRecipe UR, Image as IG
                      JOIN RecipeIngredient AS RI ON R.RecipeID = RI.RecipeID
                      WHERE R.RecipeID = UR.RecipeID AND UR.SavedStatus = 1 AND UserID = ?
                      GROUP BY R.RecipeID";
-             $stmt = $pdo->prepare($query);
-             $stmt = execute([$UserID]);
-             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $stmt = $pdo->prepare($query);
+            $stmt = execute([$UserID]);
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 echo '<div class="column1">
-                <img class="images" src="image1.jpg" alt="Recept 1">
+                <img class="images" src="'.htmlspecialchars($row['ImageURL']).'" alt="Afbeelding '.htmlspecialchars($row['RecipeTitle']) .'">
                 </div>';
                 echo '<div class="column2">';
                 echo '<h2 class="title-card">' . htmlspecialchars($row['RecipeTitle']) . '</h2>';
--- overview_include/process-filter-request.php
+++ overview_include/process-filter-request.php
@@ -2,6 +2,9 @@
 require_once('../includes/pdo-connect.php');
 require_once('../includes/config_session.php');
 
+global $preferedIngredientsString;
+global $preferedIngredientsArray;
+
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' && isset( $_POST['selectedIngredients'] ) ) {
     $preferedIngredientsString = $_POST['selectedIngredients'];
     $preferedIngredientsArray = json_decode($preferedIngredientsString);
@@ -11,19 +14,37 @@ function array_order_desc($a, $b) {
     return $b['Priority'] - $a['Priority'];
 }
 
-function displayRecipes($rows){
+function displayRecipes($rows, $preferedIngredientsArray){
     foreach ( $rows as $row ) {
         echo '<a href="recipe-page.php?recipeID=' . $row[ 'RecipeID' ] . '" class="recipe-link">';
         echo '<div class="card-holder">';
         echo '<div class="column1">';
-        echo '<img class="images" src="image1.jpg" alt="Recipe Image">';
+        echo '<img class="images" src="'.$row['ImageURL'].'" alt="Image '.$row['RecipeTitle'].'">';
         echo '</div>';
         echo '<div class="column2">';
-        echo '<h2 class="title-card">' . htmlspecialchars( $row[ 'RecipeTitle' ] ) . '</h2>';
+        echo '<h2 class="title-card">' . htmlspecialchars($row['RecipeTitle']) . '</h2>';
         echo '<div class="labels">';
         $ingredients = explode( ',', $row[ 'Ingredients' ] );
+        $ingredientsSelected = array();
+        $ingredientsNotSelected = array();
         foreach ( $ingredients as $ingredient ) {
-            echo '<span class="label-available">' . htmlspecialchars( $ingredient ) . '</span>';
+            $flag = 0;
+            foreach ( $preferedIngredientsArray as $preferedIngredient ) {
+                if ( $ingredient == $preferedIngredient) {
+                    $ingredientsSelected[] = $ingredient;
+                    $flag = 1;
+                    break;
+                }
+            }
+            if ($flag == 0) {
+                $ingredientsNotSelected[] = $ingredient;
+            }
+        }
+        foreach ( $ingredientsSelected as $selectedIngredient ) {
+            echo '<span class="label-available">' . htmlspecialchars( $selectedIngredient ) . '</span>';
+        }
+        foreach ( $ingredientsNotSelected as $notSelectedIngredient ) {
+            echo '<span class="label-unavailable">' . htmlspecialchars( $notSelectedIngredient ) . '</span>';
         }
         echo '</div>';
         echo '</div>';
@@ -36,7 +57,7 @@ function displayRecipes($rows){
 if ( $_SERVER[ 'REQUEST_METHOD' ] === 'POST' && isset( $_POST[ 'filtersApplied' ] ) ) {
     $filters = $_POST[ 'filtersApplied' ];
     // Retrieve the recipes
-    $sql = "SELECT R.RecipeID, R.Title AS RecipeTitle, GROUP_CONCAT(RI.IngredientName) AS Ingredients
+    $sql = "SELECT R.RecipeID, R.Title AS RecipeTitle, GROUP_CONCAT(RI.IngredientName) AS Ingredients, ImageURL
                 FROM Recipe R
                 JOIN RecipeIngredient AS RI ON R.RecipeID = RI.RecipeID
                 JOIN Ingredient AS I ON RI.IngredientName = I.Name
@@ -119,13 +140,12 @@ if ( $_SERVER[ 'REQUEST_METHOD' ] === 'POST' && isset( $_POST[ 'filtersApplied' 
         }
         $rows[$key]['Priority'] = $matches;
     }
-
     usort($rows, 'array_order_desc');
-    displayRecipes($rows);
+    displayRecipes($rows, $preferedIngredientsArray);
 
-    //Default display without filters
+//Default display without filters
 } else {
-    $sql = "SELECT R.RecipeID, R.Title AS RecipeTitle, GROUP_CONCAT(RI.IngredientName) AS Ingredients
+    $sql = "SELECT R.RecipeID, R.Title AS RecipeTitle, GROUP_CONCAT(RI.IngredientName) AS Ingredients, ImageURL
   FROM Recipe R
   JOIN RecipeIngredient AS RI ON R.RecipeID = RI.RecipeID
   JOIN Ingredient AS I ON RI.IngredientName = I.Name
@@ -148,6 +168,6 @@ if ( $_SERVER[ 'REQUEST_METHOD' ] === 'POST' && isset( $_POST[ 'filtersApplied' 
         $rows[$key]['Priority'] = $matches;
     }
     usort($rows, 'array_order_desc');
-    displayRecipes($rows);
+    displayRecipes($rows, $preferedIngredientsArray);
 }
 ?>
